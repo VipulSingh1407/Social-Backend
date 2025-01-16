c7939fa552aad08cda2643a52d075d69a7c0d064
VipulSingh1407 <vipulsingh9369384252@gmail.com>
2025-01-16 16:28:52 +0530

    backend

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const { submitUser, getAllUsers } = require('../controllers/userController');
-const { protectAdmin } = require('../middleware/auth');
+
 
 
 router.post('/submit', submitUser); 
